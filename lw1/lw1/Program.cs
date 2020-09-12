40ad39147e8a8aee3e7378788f47482dccd3f770
kelvionald <31734063+kelvionald@users.noreply.github.com>
2020-09-12 11:31:04 +0300

    lw1

---
+++ lw1/lw1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                try
+                {
+                    double a = Convert.ToDouble(args[0]);
+                    double b = Convert.ToDouble(args[1]);
+                    double c = Convert.ToDouble(args[2]);
+                    if (a + b <= c || a + c <= b || b + c <= a)
+                    {
+                        Console.WriteLine("не треугольник");
+                    }
+                    else if (a != b && a != c && b != c)
+                    {
+                        Console.WriteLine("обычный");
+                    }
+                    else if (a == b && b == c)
+                    {
+                        Console.WriteLine("равносторонний");
+                    }
+                    else
+                    {
+                        Console.WriteLine("равнобедренный");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("неизвестная ошибка");
+                }
+            }
+            else
+            {
+                Console.WriteLine("неизвестная ошибка");
+            }
+        }
+    }
+}
